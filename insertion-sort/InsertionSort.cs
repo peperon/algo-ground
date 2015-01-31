279c8bdec930bdcac85100e660051348440f0639
Peter Kostov <pekostov@gmail.com>
2015-01-31 10:22:29 +0200

    Added insertion sort altorithm - C# implementation

---
+++ insertion-sort/InsertionSort.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpAlgoGroundWorking
+{
+    internal class InsertionSort
+    {
+        public static void Sort(List<int> keys)
+        {
+            for (int j = 1; j < keys.Count; j++)
+            {
+                int key = keys[j];
+                int i = j - 1;
+                while (i >= 0 && keys[i] > key)
+                {
+                    keys[i + 1] = keys[i];
+                    i--;
+                }
+                keys[i + 1] = key;
+            }
+        }
+    }
+}
