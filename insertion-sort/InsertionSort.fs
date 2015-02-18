c2abd60e32b26cfbfd04863cc45186ee2dab0040
Peter Kostov <pekostov@gmail.com>
2015-02-18 21:58:37 +0200

    Add F# insertion sort

---
+++ insertion-sort/InsertionSort.fs
@@ -0,0 +1,15 @@
+﻿module InsertionSort
+
+let sort keys =
+    let rec insert el list =
+        match list with
+        | head :: tail when head >= el -> el :: list
+        | head :: tail when head < el -> head :: insert el tail
+        | _ -> [el]
+
+    let rec innerSort sorted rest =
+        match rest with
+        | head :: tail -> innerSort (insert head sorted) tail
+        | [] -> sorted
+
+    innerSort [] keys
