837c9b636aec57dd6d7b4e01a799ac0f0893efac
Peter Kostov <pekostov@gmail.com>
2015-08-02 08:24:34 +0300

    Added simple linked list implementation

---
+++ linked-list/LinkedList.cs
@@ -0,0 +1,59 @@
+﻿namespace DataStructures
+{
+    using System;
+
+    public class LinkedList<T>
+        where T : IEquatable<T>
+    {
+        private ListNode<T> _head;
+
+        public LinkedList()
+        {
+            _head = null;
+        }
+
+        public ListNode<T> Search(T value)
+        {
+            ListNode<T> current = _head;
+            while (current != null && !current.Value.Equals(value))
+            {
+                current = current.NextNode;
+            }
+
+            return current;
+        }
+
+        public void Add(T value)
+        {
+            var newNode = new ListNode<T>(value);
+            if (_head != null)
+            {
+                _head.PreviousNode = newNode;
+            }
+
+            newNode.NextNode = _head;
+            _head = newNode;
+        }
+
+        public void Delete(T value)
+        {
+            ListNode<T> forDelete = Search(value);
+            if (forDelete != null)
+            {
+                if (forDelete.PreviousNode != null)
+                {
+                    forDelete.PreviousNode.NextNode = forDelete.NextNode;
+                }
+                else
+                {
+                    _head = forDelete.NextNode;
+                }
+
+                if (forDelete.NextNode != null)
+                {
+                    forDelete.NextNode.PreviousNode = forDelete.PreviousNode;
+                }
+            }
+        }
+    }
+}
---
+++ linked-list/ListNode.cs
@@ -0,0 +1,21 @@
+﻿namespace DataStructures
+{
+    using System;
+
+    public class ListNode<T>
+        where T : IEquatable<T>
+    {
+        public ListNode(T value)
+        {
+            Value = value;
+            PreviousNode = null;
+            NextNode = null;
+        }
+
+        public T Value { get; set; }
+
+        public ListNode<T> PreviousNode { get; set; }
+
+        public ListNode<T> NextNode { get; set; }
+    }
+}
