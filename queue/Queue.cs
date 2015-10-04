fbdefeb49c2a395d09f4090d8643b8ac73469127
Peter Kostov <pekostov@gmail.com>
2015-10-04 10:45:32 +0300

    Added simple queue implementation

---
+++ queue/Queue.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class Queue<T>
+    {
+        private QueueElement<T> _head;
+        private QueueElement<T> _tail;
+
+        public Queue()
+        {
+            _head = null;
+            _tail = null;
+        }
+
+        public bool IsEmpty()
+        {
+            return _head == null;
+        }
+
+        public void Enqueue(T value)
+        {
+            var newElement = new QueueElement<T>(value);
+            if (IsEmpty())
+            {
+                _head = _tail = newElement;
+            }
+            else
+            {
+                _tail.Previous = newElement;
+                _tail = newElement;
+            }
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("The queue is empty.");
+
+            T value = _head.Value;
+
+            _head = _head.Previous;
+            if (_head == null)
+            {
+                _tail = null;
+            }
+
+            return value;
+        }
+    }
+}
+
---
+++ queue/QueueElement.cs
@@ -0,0 +1,16 @@
+﻿namespace DataStructures
+{
+    public class QueueElement<T>
+    {
+        public QueueElement(T value)
+        {
+            Value = value;
+            Previous = null;
+        }  
+
+        public T Value { get; set; }
+
+        public QueueElement<T> Previous { get; set; }
+    }
+}
+
